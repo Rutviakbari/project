df6930a5b82944e7e2684e259fff1d1812777158
Rutviakbari <100115715+Rutviakbari@users.noreply.github.com>
2022-02-22 09:47:37 +0430

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,12 @@
+a=int(input("enter number"))
+if a>1:
+  for x in range(2,a):
+    if(a%x)==0:
+        print("not prime")
+        break
+    else:
+        print("prime")
+        break
+else:
+        print("not prime")
+    
